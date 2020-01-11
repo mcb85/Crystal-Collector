a8b6d3989a63d161552977e69582e9cfdfd3ffb5
Mariana <mariana.diaz@live.com>
2020-01-11 00:17:06 -0500

    began styling html page

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+let counter = 0; 
+$(".crystal-image").on("click", function() {
+    counter += 1;
+    alert("You clicked this crystal!" + counter + "times!");    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
